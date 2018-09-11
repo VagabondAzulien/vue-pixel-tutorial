28e980a0d2fe19a519907dfe92adad723b973ec1
Bill Niblock <azulien@gmail.com>
2018-09-11 19:26:12 -0400

    Add project files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <ColorPicker :color=color />
+        <Canvas :pixels=pixels />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Canvas from './components/Canvas.vue'
+import ColorPicker from './components/ColorPicker'
+
+const defaultPixelColor = 'white'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    data: function() {
+        return {
+            color: defaultPixelColor,
+            pixels: Array(30 * 30).fill().map(() => defaultPixelColor)
+        }
+    },
+    components: {
+        Canvas,
+        ColorPicker
+    },
+    mounted() {
+        this.$root.$on('updatecolor', color => {
+            this.color = color
+        })
+        this.$root.$on('clickedpixel', index => {
+            this.pixels.splice(index, 1, this.color)
+        })
+    }
 }
 </script>
 
 <style>
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    background-color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
 }
 </style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="canvas">
+        <Pixel isInCanvas 
+            v-for="(color, index) in pixels"
+            :key="index" 
+            :color="color" 
+            :index="index"
+        />
+    </div>
+</template>
+
+<script>
+import Pixel from './Pixel'
+
+export default {
+    name: 'Canvas',
+    components: {
+        Pixel
+    },
+    props: {
+        pixels: String, Number
+    }
+}
+</script>
+
+<style scoped>
+.canvas {
+    display: flex;
+    flex-wrap: wrap;
+    max-width: 900px;
+}
+</style>
